0b46e22099c61a63aa968d893f83d0a337ed436d
Vanessa Jesik <vajesik@gmail.com>
2023-09-13 10:22:45 -0600

    completed lab

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    evens_list = [num for num in num_list if num % 2 == 0]
+    return evens_list
 
 def make_exclamation(sentence_list):
-    pass
+    exclamation_list = [string + "!" for string in sentence_list]
+    return exclamation_list
